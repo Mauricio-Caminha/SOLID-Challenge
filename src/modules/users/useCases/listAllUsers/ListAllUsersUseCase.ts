f8df34334450826e8c262690e1e8a53483cf1a14
Maurício <mauricio.caminha@hotmail.com>
2023-01-29 15:52:01 -0300

    Challenge finished

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,14 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userExists = this.usersRepository.findById(user_id);
+
+    if (userExists.admin) {
+      const users = this.usersRepository.list();
+      return users;
+    }
+
+    throw new Error("Forbidden.");
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,15 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const turnAdminUser = this.turnUserAdminUseCase.execute({ user_id });
+
+      return response.status(200).json(turnAdminUser);
+    } catch (error) {
+      return response.status(404).json({ error });
+    }
   }
 }
 
